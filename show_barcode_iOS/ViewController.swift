ac32c9b32490892d130ca4d217d837dacdd2a934
Takahiro Tsuchiya <sample@sample.com>
2019-09-06 16:33:00 +0900

    Initial Commit

---
+++ show_barcode_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  show_barcode_iOS
+//
+//  Created by tatsuchi on 2019/09/06.
+//  Copyright © 2019 Takahiro Tsuchiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
